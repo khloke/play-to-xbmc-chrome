bc38fc0f855c4c293f1ab2cabb89ec1adce9c4b7
khloke <khloke@gmail.com>
2014-01-24 22:37:39 +1000

    -Added video file extensions to context menu support

--- js/content_scripts/context-menu-content-scripts.js
+++ js/content_scripts/context-menu-content-scripts.js
@@ -1,6 +1,9 @@
-var musicExtensions = ['mp3', 'ogg', 'midi', 'wav', 'aiff', 'aac', 'flac', 'ape', 'wma'];
+var supportedExtensions = [
+    'mp3', 'ogg', 'midi', 'wav', 'aiff', 'aac', 'flac', 'ape', 'wma', 'm4a', 'mka',        //Audio extensions
+    'avi', 'wmv', 'asf', 'flv', 'mkv', 'mp4'                                               //Video extension
+];
 
-// Port management
+
 var _port;
 var getPort = function() {
     if (_port) return _port;
@@ -11,12 +14,7 @@ var getPort = function() {
     return _port;   
 }
 
-// listOfCreateProperties is an array of createProperties, which is defined at
-// https://developer.chrome.com/extensions/contextMenus.html#method-create
-// with a single exception: "onclick" is a string which corresponds to a function
-// at the background page. (Functions are not JSON-serializable, hence this approach)
 function addContextMenuTo(selector, listOfCreateProperties) {
-    // Selector used to match an element. Match if an element, or its child is hovered
     selector = selector + ', ' + selector + ' *';
     var matches;
     ['matches', 'webkitMatchesSelector', 'webkitMatches', 'matchesSelector'].some(function(m) {
@@ -25,7 +23,7 @@ function addContextMenuTo(selector, listOfCreateProperties) {
             return true;
         }
     });
-    // Bind a single mouseover+mouseout event to catch hovers over all current and future elements.
+
     var isHovering = false;
     document.addEventListener('mouseover', function(event) {
         if (event.target && event.target[matches](selector)) {
@@ -44,48 +42,46 @@ function addContextMenuTo(selector, listOfCreateProperties) {
     });
 }
 
-// Example: Bind the context menus to the elements which contain a class attribute starts with "story"
-
 function concatMusicExtensions() {
     var concatSelector = '';
     var first = true;
-    for (var i=0;i<musicExtensions.length;i++) {
+    for (var i=0;i<supportedExtensions.length;i++) {
         if (first) {
             first = false;
         } else {
             concatSelector += ',';
         }
-        concatSelector += "a[href$='." + musicExtensions[i] + "']"
+        concatSelector += "a[href$='." + supportedExtensions[i] + "']"
     }
     return concatSelector;
 }
 
-//function checkConnectivity(callback) {
-//    chrome.extension.sendMessage({action: 'isAvailable'}, function (response) {
-//        if (response.response == 'OK') {
-//            callback(true);
-//        } else {
-//            callback(false);
-//        }
-//    });
-//}
-//
-//checkConnectivity(function(isAvailable) {
-//    if (isAvailable) {
-//        addContextMenuTo(concatMusicExtensions(),
-//            [{
-//                title: "Play now",
-//                contexts: ["link"],
-//                onclick: 'musicPlayNow'
-//            },{
-//                title: "Queue",
-//                contexts: ["link"],
-//                onclick: 'musicPlayNow'
-//            },{
-//                title: "Play this Next",
-//                contexts: ["link"],
-//                onclick: 'musicPlayNow'
-//            }]
-//        );
-//    }
-//});
+function checkConnectivity(callback) {
+    chrome.extension.sendMessage({action: 'isAvailable'}, function (response) {
+        if (response.response == 'OK') {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+}
+
+checkConnectivity(function(isAvailable) {
+    if (isAvailable) {
+        addContextMenuTo(concatMusicExtensions(),
+            [{
+                title: "Play now",
+                contexts: ["link"],
+                onclick: 'musicPlayNow'
+            },{
+                title: "Queue",
+                contexts: ["link"],
+                onclick: 'musicPlayNow'
+            },{
+                title: "Play this Next",
+                contexts: ["link"],
+                onclick: 'musicPlayNow'
+            }]
+        );
+    }
+});
