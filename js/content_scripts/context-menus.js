27d70f55868b76c86e513a2e3459534d60d39562
khloke <khloke@gmail.com>
2014-06-30 22:57:50 +1000

    -Rather than run script on document ready, run it as soon as the DOM is done loading. This might fix the problem with inconsistent context menus.

--- js/content_scripts/context-menus.js
+++ js/content_scripts/context-menus.js
@@ -1,26 +1,24 @@
-$(document).ready(function() {
-    $("a").each(function() {
-        var linkUrl = $(this).attr('href');
-        if (linkUrl && linkUrl.match('^//.*$')) {
-            var tabUrl = window.location.href;
-            var patternMatch = tabUrl.match('^(https|http)://(.+)/.*$');
-            if (patternMatch) {
-                chrome.extension.sendMessage({action: 'createContextMenu', link: patternMatch[1] + ':' + linkUrl}, function (response) {});
-            } else {
-                console.log("Could not determine what to do with link: " + linkUrl);
-            }
-        } else if (linkUrl && linkUrl.match('^/.*$')) {
-            var tabUrl = window.location.href;
-            var patternMatch = tabUrl.match('^(https|http)://(.+)/.*$');
-            if (patternMatch) {
-                chrome.extension.sendMessage({action: 'createContextMenu', link: patternMatch[1] + '://' + patternMatch[2] + linkUrl}, function (response) {});
-            } else {
-                console.log("Could not determine what to do with link: " + linkUrl);
-            }
-        } else if (linkUrl && linkUrl.match('^(https|http)://.+$')) {
-            chrome.extension.sendMessage({action: 'createContextMenu', link: linkUrl}, function (response) {});
+$("a").each(function() {
+    var linkUrl = $(this).attr('href');
+    if (linkUrl && linkUrl.match('^//.*$')) {
+        var tabUrl = window.location.href;
+        var patternMatch = tabUrl.match('^(https|http)://(.+)/.*$');
+        if (patternMatch) {
+            chrome.extension.sendMessage({action: 'createContextMenu', link: patternMatch[1] + ':' + linkUrl}, function (response) {});
         } else {
             console.log("Could not determine what to do with link: " + linkUrl);
         }
-    });
+    } else if (linkUrl && linkUrl.match('^/.*$')) {
+        var tabUrl = window.location.href;
+        var patternMatch = tabUrl.match('^(https|http)://(.+)/.*$');
+        if (patternMatch) {
+            chrome.extension.sendMessage({action: 'createContextMenu', link: patternMatch[1] + '://' + patternMatch[2] + linkUrl}, function (response) {});
+        } else {
+            console.log("Could not determine what to do with link: " + linkUrl);
+        }
+    } else if (linkUrl && linkUrl.match('^(https|http)://.+$')) {
+        chrome.extension.sendMessage({action: 'createContextMenu', link: linkUrl}, function (response) {});
+    } else {
+        console.log("Could not determine what to do with link: " + linkUrl);
+    }
 });
