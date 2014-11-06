5751cfad5c8aa4f7c75bbdac0f923d413004eb78
khloke <khloke@gmail.com>
2014-11-06 21:39:10 +1000

    Change link detection back to onmouseover rather than at the start of the page. This is more reliable in detecting links that were loading afterwards.

--- js/content_scripts/context-menu-content-scripts.js
+++ js/content_scripts/context-menu-content-scripts.js
@@ -1,5 +1,33 @@
-$("a").each(function() {
-    var linkUrl = $(this).attr('href');
+function addContextMenuTo(selector) {
+    selector = selector + ', ' + selector + ' *';
+    var matches;
+    ['matches', 'webkitMatchesSelector', 'webkitMatches', 'matchesSelector'].some(function(m) {
+        if (m in document.documentElement) {
+            matches = m;
+            return true;
+        }
+    });
+
+    var isHovering = false;
+    document.addEventListener('mouseover', function(event) {
+        if (event.target && event.target[matches](selector)) {
+            createContextMenu(event.target.href);
+//            getPort().postMessage(listOfCreateProperties);
+            isHovering = true;
+        } else if (isHovering) {
+//            getPort().postMessage([]);
+            isHovering = false;
+        }
+    });
+    document.addEventListener('mouseout', function(event) {
+        if (isHovering && (!event.target || !event.target[matches](selector))) {
+//            getPort().postMessage([]);
+            isHovering = false;
+        }
+    });
+}
+
+function createContextMenu(linkUrl) {
     if (linkUrl && linkUrl.match('^//.*$')) {
         var tabUrl = window.location.href;
         var patternMatch = tabUrl.match('^(https|http)://(.+)/.*$');
@@ -18,12 +46,11 @@ $("a").each(function() {
         }
     } else if (linkUrl && linkUrl.match('^(https|http)://.+$')) {
         chrome.extension.sendMessage({action: 'createContextMenu', link: linkUrl}, function (response) {});
-    } else if (linkUrl && (
-        linkUrl.indexOf('#') == 0 ||
-            linkUrl.indexOf('mailto') == 0)
-        ) {
+    } else if (!linkUrl || (linkUrl && (linkUrl.indexOf('#') == 0 || linkUrl.indexOf('mailto') == 0 || linkUrl.indexOf('javascript') == 0))) {
         //Do nothing to these links.
     } else {
         console.log("Could not determine what to do with link: " + linkUrl);
     }
-});
+}
+
+addContextMenuTo('a');
