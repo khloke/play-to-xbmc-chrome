d07fc1b0dc0dad7a46d358a54185eae844510137
Khee Hong Loke <khloke@gmail.com>
2015-02-26 08:38:55 +1000

    Merge changes after resolving conflicts

---
+++ js/content_scripts/kinolive-content-scripts.js
@@ -0,0 +1,12 @@
+
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if ("getKinoLiveVideoUrl" == request.action) {
+            var videoLink = $('html').html().match('file=(https?\\:.+?\\.flv)')[1];
+            if (videoLink) {
+                sendResponse({url: videoLink});
+            }
+        }
+    }
+);
+
