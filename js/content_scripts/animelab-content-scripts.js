9f0d5984b7f331970e7893ecb9dfbe612c28f603
khloke <khloke@gmail.com>
2015-06-14 22:03:44 +1000

    Auto pause video when sending it to Kodi

--- js/content_scripts/animelab-content-scripts.js
+++ js/content_scripts/animelab-content-scripts.js
@@ -3,7 +3,9 @@
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action == "getVideoSrc") {
-            var videoSrc = $('video').attr('src');
+            var $video = $('video');
+            var videoSrc = $video.attr('src');
+            $video[0].pause();
             sendResponse({videoSrc: videoSrc});
         }
     }
--- js/content_scripts/vessel-content-scripts.js
+++ js/content_scripts/vessel-content-scripts.js
@@ -4,6 +4,7 @@ chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action == "getVideoSrc") {
             var videoSrc = decodeURIComponent($('video.video-show').attr('src'));
+            $('video')[0].pause();
             sendResponse({videoSrc: videoSrc});
         }
     }
