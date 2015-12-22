b6750c6ab37d9834e3496ae38f38595405ada37e
khloke <khloke@gmail.com>
2015-12-22 22:15:46 +1000

    [#81] Fixed regex to ignore query strings on direct video links.

--- js/modules.js
+++ js/modules.js
@@ -14,7 +14,7 @@ var DirectVideoLinkModule = {
         var supportedVideoExtensions = ['avi', 'wmv', 'asf', 'flv', 'mkv', 'mp4', 'webm'];
         for (var i = 0; i < supportedVideoExtensions.length; i++) {
             var extension = supportedVideoExtensions[i];
-            var regex = '^.*\.' + extension + "$";
+            var regex = '^.*\.(' + extension + '|' + extension + '\?.+)$';
             if (url.match(regex)) {
                 return true;
             }
