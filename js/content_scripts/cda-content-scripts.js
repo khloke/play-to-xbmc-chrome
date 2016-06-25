8926af9b9bf17afb7d9f3beae2ab08e76062227e
Grzegorz Makarewicz <mak@trisoft.com.pl>
2016-06-25 19:45:03 +0200

    popup dialog with links

--- js/content_scripts/cda-content-scripts.js
+++ js/content_scripts/cda-content-scripts.js
@@ -18,11 +18,67 @@ function findUrls( text )
     return urlArray;
 }
 
+function showdialog(urls, done) {
+//    debugger;
+    var buttons = [];
+    for (var i = 0; i < urls.length; i++) {
+        if (urls[i].indexOf('.flv') > 0 || urls[i].indexOf('.mp4') > 0) {
+            var url = urls[i];
+            buttons.push({
+                text: url,
+                click: function() {
+                    done(url);
+                    $(this).dialog("close");
+                }
+            });
+        }
+    }
+
+    var layerNode= document.createElement('div');
+    layerNode.setAttribute('id','x-kodi-cda-dialog');
+    layerNode.setAttribute('title','Links information');
+/*
+    var pNode= document.createElement('p');
+    pNode.innerHTML  = "<b>woow</b>";
+    layerNode.appendChild(pNode);
+*/
+    document.body.appendChild(layerNode);
+
+    jQuery("#x-kodi-cda-dialog").dialog({
+        autoOpen: true, 
+        draggable: true,
+        resizable: true,
+        height: 'auto',
+        width: "800",
+        zIndex:3999,
+        modal: false,
+        closeOnEscape: true,
+        buttons: buttons,
+        open: function(event, ui) {
+            $(event.target).parent().css('position','fixed');
+            $(event.target).parent().css('top', '5px');
+            $(event.target).parent().css('left', '10px');
+        }
+    });
+}
+
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log("Received message: " + request.action);
         if (request.action == "getEmbeddedVideos" || request.action == "getVideoSrc") {
 //            debugger;
+            try {
+                var url = $('video').attr('src');
+                if( url === undefined )
+                    throw "url is undefined";
+                url = decodeURIComponent(url);
+                showdialog([url], function(url) {
+                    sendResponse({videoSrc: url});
+                });
+                return;
+            } catch (e) {
+                console.log("not html5 player:" + e);
+            };
             var stext = $('#player').text();
             var es = stext.search('eval');
             var ei = stext.slice(es).search('return p');
@@ -47,13 +103,10 @@ chrome.extension.onMessage.addListener(
             }
             var result = pp(eo[0], eo[1], eo[2], eo[3], eo[4], eo[5]);
             var urls = findUrls(result);
-            for (var i = 0; i < urls.length; i++) {
-                if (urls[i].indexOf('.flv') >= 0 || urls[i].indexOf('.mp4') >= 0) {
-                    var videoSrc = decodeURIComponent(urls[i]);
+            showdialog(urls, function(url) {
+                    var videoSrc = decodeURIComponent(url);
                     sendResponse({videoSrc: videoSrc});
-                    return;
-                }
-            }
+            });
         } else {
             console.log('Unknown action: ' + request.action);
         }
